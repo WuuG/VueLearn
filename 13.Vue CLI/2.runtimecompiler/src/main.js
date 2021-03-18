6c47bf544a3c01fb3bf1d2fa1e21432a09ba74af
Wuu-laptop <1249732457@qq.com>
2021-03-18 17:50:45 +0800

    runtime-conpiler 和runtime-only的区别。以及render函数的使用

--- 13.Vue CLI/2.runtimecompiler/src/main.js
+++ 13.Vue CLI/2.runtimecompiler/src/main.js
@@ -6,8 +6,39 @@ import App from './App'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+const cpn = {
+  template: `
+  <div>
+    <h1>{{message}}</h1>
+  </div>
+  `,
+  data() {
+    return {
+      message:'这是测试变量在render中的使用'
+    }
+  }
+}
+
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  // components: { App },
+  // template: '<App/>'
+  render: function (createElement) {
+    // 1.createElement('标签',{标签的属性},['内容'])
+    // return createElement('h2', { class: 'box' }, ['hello vue'])
+    //1.1 可以嵌套使用
+    // return createElement('h2', 
+    // { class: 'box' },
+    // ['hello vue',createElement('button',['子项的按钮'])])
+    //2.createElement(组件对象)
+    // return createElement(cpn)
+    return createElement(App)
+  }
 })
+
+// 1.runtiem-compiler
+// template-> ast->render->vdon->ui
+
+// 2.runtime-only  (1.性能更高。2.代码量更少。)
+// render->vdon->ui
--- 13.Vue CLI/3.runtime/src/main.js
+++ 13.Vue CLI/3.runtime/src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
-
+console.log(App);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+//这里传过来的App对象中，已经没有template了
+//因为vue-template-compiler将template转换成render函数了
+
