89eeb9f9ddcf910a70ffaabc599429ecd26d2083
Wuu-laptop <1249732457@qq.com>
2021-03-20 14:18:14 +0800

    vuex的项目构建

---
+++ 16.Vuex/1.vuex/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+    <button @click="count++">+</button>
+    <h1>{{ count }}</h1>
+    <button @click="count--">-</button>
+    <hello-vuex :message="forVuex"></hello-vuex>
+  </div>
+</template>
+
+<script>
+import helloVuex from "@/components/HelloVuex.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "这是来自app的消息",
+      forVuex: "这是传送给Vuex的消息",
+      count: 0,
+    };
+  },
+  components: {
+    helloVuex,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+button {
+  height: 40px;
+  width: 40px;
+}
+</style>
---
+++ 16.Vuex/1.vuex/src/components/HelloVuex.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- <h2>{{ count }}</h2> -->
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    message: {
+      type: String,
+      dafault: "这是Vuex的默认消息",
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
